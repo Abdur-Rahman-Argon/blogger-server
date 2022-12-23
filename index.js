c804e9e797df7f36f13a114d0a98b977b595baaa
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2022-12-23 18:37:37 +0600

    server deploy

--- index.js
+++ index.js
@@ -10,9 +10,18 @@ app.get("/", (req, res) => {
   res.send(" Blogging server is Running");
 });
 
-app.get("/product", (req, res) => {
+app.get("/all-blogs", (req, res) => {
   res.send("product");
 });
+// app.get("/my-blogs", (req, res) => {
+//   res.send("product");
+// });
+// app.get("/create-new-blogs", (req, res) => {
+//   res.send({ status: 200, success: true, insertedId: "id" });
+// });
+// app.get("/update-blogs", (req, res) => {
+//   res.send({ status: 200, success: true, upsert: "", insertedId: "id" });
+// });
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
