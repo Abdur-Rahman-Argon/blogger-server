b2ee40bc1724f70e63974287cb1d11be02152b5b
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2022-12-23 22:26:08 +0600

    mongoDb Connect

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MongoDB_User}:${process.env.MongoDB_PASS}@cluster0.ugwei28.mongodb.net/?retryWrites=true&w=majority
+`;
+
+/* const uri = `mongodb+srv://my-blogging-database:206YxbGwGa5dtwdV@cluster0.ugwei28.mongodb.net/?retryWrites=true&w=majority`;
+ */
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    client.connect();
+
+    const blogsCollections = client.db("blogs").collection("blogs-collection");
+    console.log("connect");
+
+    app.get("/all-blogs", async (req, res) => {
+      const query = {};
+      const cursor = await blogsCollections.find(query);
+      const blogs = await cursor.toArray();
+      res.send(blogs);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send(" Blogging server is Running");
